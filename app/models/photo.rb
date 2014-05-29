4e1d470a45c55285e5a36ae7514cf47311d5ea07
alex greenwood <alexander.greenwood@gmail.com>
2014-05-28 20:17:24 -0500

    added validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,5 +6,7 @@ has_many :favorites
 
 has_many :fans, :through => :favorites, :source => :user
 
+validates :image_url, :presence => true, :uniqueness => { :scope => :caption }
+validates :user_id, :presence => true
 
 end
